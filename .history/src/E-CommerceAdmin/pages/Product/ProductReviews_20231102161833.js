8b27dfa1c0dc526699b6693b6e0996287161ca6f
React1 <React1@flyweis.technology>
2023-11-02 19:08:28 +0530

    7:20

---
+++ .history/src/E-CommerceAdmin/pages/Product/ProductReviews_20231102161833.js
@@ -0,0 +1,18 @@
+
+import React, { useEffect, useState } from "react";
+import HOC from "../../layout/HOC";
+import { Table, Alert } from "react-bootstrap";
+import { toast } from "react-toastify";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import axios from "axios";
+import SpinnerComp from "../Component/SpinnerComp";
+import { Link } from "react-router-dom";
+import { Baseurl } from "../../../Baseurl";
+
+const ProductReviews = () => {
+  return (
+    <div>ProductReviews</div>
+  )
+}
+
+export default ProductReviews
---
+++ .history/src/E-CommerceAdmin/pages/Product/ProductReviews_20231102162025.js
@@ -0,0 +1,142 @@
+/** @format */
+
+import React, { useEffect, useState } from "react";
+import HOC from "../../layout/HOC";
+import { Table, Alert } from "react-bootstrap";
+import { toast } from "react-toastify";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import axios from "axios";
+import SpinnerComp from "../Component/SpinnerComp";
+import { Link, useParams } from "react-router-dom";
+import { Baseurl } from "../../../Baseurl";
+
+const ProductReviews = () => {
+    const { id } = useParams()
+  const [data, setData] = useState([]);
+
+  const token = localStorage.getItem("AdminToken");
+  const Auth = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(
+        `${Baseurl}api/v1/product/getProductReviews/${id}`
+      );
+      setData(data.reviews);
+      setTotal(data.reviews.total);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const deleteHandler = async (id) => {
+    try {
+      const { data } = await axios.delete(
+        `${Baseurl}api/v1/Product/deleteProduct/${id}`,
+        Auth
+      );
+      toast.success(data.message);
+      fetchData();
+    } catch (e) {
+      const msg = e.response.data.message;
+      toast.error(msg);
+    }
+  };
+
+  return (
+    <>
+      <section className="sectionCont">
+        <div className="pb-4  w-full flex justify-between items-center">
+          <span
+            className="tracking-widest text-slate-900 font-semibold uppercase"
+            style={{ fontSize: "1.5rem" }}
+          >
+            All Product Reviews ( Total : {total} )
+          </span>
+     
+        </div>
+
+    
+            <div className="overFlowCont">
+              {data?.length === 0 || !data ? (
+                <Alert>No Review Found</Alert>
+              ) : (
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>Sno.</th>
+                      <th>Image</th>
+                      <th>Title</th>
+                      <th>Reviews</th>
+                      <th> </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data?.docs?.map((i, index) => (
+                      <tr key={index}>
+                        <td> #{index + 1} </td>
+                        <td style={{ cursor: "pointer" }}>
+                          <div className="CarouselImages">
+                            <img src={i.productImages?.[0]?.image} alt="" />
+                          </div>
+                        </td>
+                        <td> {i.name} </td>
+                        <td>
+                          {i.reviews?.length > 0 && (
+                            <Link to="/create-product">
+                              <button className="md:py-2 px-3 md:px-4 py-1 rounded-sm bg-[#042b26] text-white tracking-wider">
+                                View
+                              </button>
+                            </Link>
+                          )}
+                        </td>
+
+                        <td>
+                          <span className="flexCont">
+                            <Link to={`/edit-product/${i._id}`}>
+                              <i className="fa-solid fa-pen-to-square" />
+                            </Link>
+                            <Link to={`/product/${i._id}`}>
+                              <i className="fa-solid fa-eye" />
+                            </Link>
+                            <i
+                              className="fa-sharp fa-solid fa-trash"
+                              onClick={() => deleteHandler(i._id)}
+                            ></i>
+                          </span>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              )}
+
+              <div className="pagination">
+                <button onClick={() => Prev()} className="prevBtn">
+                  <i className="fa-solid fa-backward"></i>
+                </button>
+
+                <button className="activePage">{page}</button>
+
+                <button onClick={() => Next()} className="nextBtn">
+                  {" "}
+                  <i className="fa-sharp fa-solid fa-forward"></i>
+                </button>
+              </div>
+            </div>
+        
+      </section>
+    </>
+  );
+};
+
+export default HOC(ProductReviews);
