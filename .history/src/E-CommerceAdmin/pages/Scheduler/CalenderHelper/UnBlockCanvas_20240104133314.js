5c0969e5add89290c21d06cc8f53664416ae63d5
React1 <React1@flyweis.technology>
2024-01-04 17:01:15 +0530

    chnages

---
+++ .history/src/E-CommerceAdmin/pages/Scheduler/CalenderHelper/UnBlockCanvas_20240104133314.js
@@ -0,0 +1,112 @@
+/** @format */
+import { useState } from "react";
+import { Offcanvas } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { createBlockedTime, unblock_slot } from "../../../../Respo/Api";
+import { selectModalById } from "../../../../Store/Slices/modalSlices";
+
+const UnBlockCanvas = ({ show, handleClose, fetchHandler }) => {
+  const { modalData } = useSelector(selectModalById("unblockedCanvas"));
+
+  const start = new Date(modalData?.start);
+  const Day = start?.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
+  const month = start?.toLocaleDateString("en-US", {
+    month: "long",
+  });
+  const year = start?.toLocaleDateString("en-US", {
+    year: "numeric",
+  });
+  const date = start?.toLocaleDateString("en-US", {
+    day: "numeric",
+  });
+
+  const startingTime = start?.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const end = new Date(modalData?.end);
+  const endingTime = end?.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const value =
+    Day?.slice(0, 3) + " , " + date + " " + month?.slice(0, 3) + " " + year;
+
+  const timeValue = startingTime + " to " + endingTime;
+
+  // ----
+  const monthFormated = parseInt(start?.getMonth()) + 1;
+  const dayFormated = start?.getDate();
+  const monthStr = monthFormated < 10 ? `0${monthFormated}` : monthFormated;
+  const dayStr = dayFormated < 10 ? `0${dayFormated}` : dayFormated;
+
+  const formatedDate = `${year}-${monthStr}-${dayStr}`;
+  const from = startingTime?.slice(0, 5);
+  const to = endingTime?.slice(0, 5);
+  // ----
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const submiHandler = (e) => {
+    e.preventDefault();
+    const payload = {
+      date: formatedDate,
+      to: from,
+      from: to,
+    };
+    unblock_slot(payload)
+  };
+
+  return (
+    <Offcanvas
+      show={show}
+      onHide={handleClose}
+      placement="end"
+      style={{ width: "100%" }}
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title style={{ fontWeight: "900" }}>
+          Unblock slot
+        </Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body className="booked_appointment_modal">
+        <form onSubmit={submiHandler}>
+          <div>
+            <p>Title</p>
+            <input
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="e.g. lunch meeting (optional)"
+            />
+          </div>
+          <div>
+            <p>Date</p>
+            <input type="text" defaultValue={value} />
+          </div>
+          <div>
+            <p>Time</p>
+            <input type="text" defaultValue={timeValue} />
+          </div>
+
+          <div>
+            <p>Description</p>
+            <input
+              type="text"
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Add description or note (optional)"
+            />
+          </div>
+
+          <button type="submit"> Save</button>
+        </form>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
+};
+
+export default UnBlockCanvas;
+
+
---
+++ .history/src/E-CommerceAdmin/pages/Scheduler/CalenderHelper/UnBlockCanvas_20240104133445.js
@@ -0,0 +1,97 @@
+/** @format */
+import { useState } from "react";
+import { Offcanvas } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { createBlockedTime, unblock_slot } from "../../../../Respo/Api";
+import { selectModalById } from "../../../../Store/Slices/modalSlices";
+
+const UnBlockCanvas = ({ show, handleClose, fetchHandler }) => {
+  const { modalData } = useSelector(selectModalById("unblockedCanvas"));
+
+  const start = new Date(modalData?.start);
+  const Day = start?.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
+  const month = start?.toLocaleDateString("en-US", {
+    month: "long",
+  });
+  const year = start?.toLocaleDateString("en-US", {
+    year: "numeric",
+  });
+  const date = start?.toLocaleDateString("en-US", {
+    day: "numeric",
+  });
+
+  const startingTime = start?.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const end = new Date(modalData?.end);
+  const endingTime = end?.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const value =
+    Day?.slice(0, 3) + " , " + date + " " + month?.slice(0, 3) + " " + year;
+
+  const timeValue = startingTime + " to " + endingTime;
+
+  const endingTime2 = end?.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
+
+  // ----
+  const monthFormated = parseInt(start?.getMonth()) + 1;
+  const dayFormated = start?.getDate();
+  const monthStr = monthFormated < 10 ? `0${monthFormated}` : monthFormated;
+  const dayStr = dayFormated < 10 ? `0${dayFormated}` : dayFormated;
+  const formatedDate = `${year}-${monthStr}-${dayStr}`;
+  const from = startingTime?.slice(0, 5);
+  const to = endingTime?.slice(0, 5);
+  // ----
+
+  const submiHandler = (e) => {
+    e.preventDefault();
+    const payload = {
+      date: formatedDate,
+      to: endingTime2,
+      from: from,
+    };
+    unblock_slot(payload);
+  };
+
+  return (
+    <Offcanvas
+      show={show}
+      onHide={handleClose}
+      placement="end"
+      style={{ width: "100%" }}
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title style={{ fontWeight: "900" }}>
+          Unblock slot
+        </Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body className="booked_appointment_modal">
+        <form onSubmit={submiHandler}>
+          <div>
+            <p>Date</p>
+            <input type="text" defaultValue={value} />
+          </div>
+          <div>
+            <p>Time</p>
+            <input type="text" defaultValue={timeValue} />
+          </div>
+
+          <button type="submit"> Save</button>
+        </form>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
+};
+
+export default UnBlockCanvas;
